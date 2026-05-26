d0b26f0486f4a09215b1a0bdc910a75091dadcfe
Jonathan Faasse <jonathanfaasse@live.nl>
2026-05-26 13:32:27 +0200

    add parametrized daily_mean test

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,28 +6,17 @@ import numpy.testing as npt
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-    
+@pytest.mark.parametrize(
+        "test_input, test_result",
+        [
+            ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+            ([[1,2], [3, 4], [5, 6]], [3, 4])
+        ]        
+)
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+def test_daily_mean(test_input, test_result):
+    """ Test that daily mean function works for given test_input and test_result"""
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 def test_daily_mean_strings():
@@ -35,6 +24,16 @@ def test_daily_mean_strings():
 
     with pytest.raises(TypeError):
         error_expected = daily_max(["Hello", "there"])
+
+def test_daily_mean_single_integer():
+    """ Test that mean function returns a single integer input """
+
+    test_input = 3
+    
+    with npt.assert_raises(np.exceptions.AxisError):
+        daily_mean(test_input)
+
+
 
 def test_daily_max_integers():
     """Test that max function works for an array of integers."""
