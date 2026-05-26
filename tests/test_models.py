4ee887aa4f1481ffee63b064d8f712ec021b02b8
Jonathan Faasse <jonathanfaasse@live.nl>
2026-05-26 11:49:32 +0200

    add tests for daily_max and daily_min

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max, daily_min
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +28,24 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_max_integers():
+    """Test that max function works for an array of integers."""
+
+    test_input = np.array([[1, 2], 
+                           [-3, 4], 
+                           [5, 0]])
+    test_result = np.array([5, 4])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_integers():
+    """Test that min function works for an array of integers."""
+
+    test_input = np.array([[1, 2], 
+                           [-3, 4], 
+                           [5, 0]])
+    test_result = np.array([-3, 0])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
 
